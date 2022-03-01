bb0ed3de7b007e50877c752f051dacba43508b75
Jonathan Green <jonathan@razzard.com>
2022-02-28 21:41:08 -0400

    Update plugin source

--- src/poetry_scm_version/plugin.py
+++ src/poetry_scm_version/plugin.py
@@ -4,7 +4,6 @@ import wrapt
 from cleo.io.io import IO
 from poetry.console.application import Application
 from poetry.core.packages.project_package import ProjectPackage
-from poetry.core.semver.version import Version
 from poetry.factory import Factory
 from poetry.plugins import ApplicationPlugin, Plugin
 from poetry.poetry import Poetry
@@ -18,10 +17,8 @@ def wrapper(
 ) -> ProjectPackage:
     name, version = args
     if version == "scm-version":
-        parsed_version = Version.from_parts(0)
-        return ProjectPackage(name, parsed_version, version)
-    else:
-        return wrapped(name, version)
+        version = "0"
+    return wrapped(name, version)
 
 
 class ScmPatchPlugin(ApplicationPlugin):
